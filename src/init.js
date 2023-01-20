75a4448dd6c05eb50c44a4a5af51532d132f0778
Roman Sukhopleshchenko <roman.sukhopleshchenko@yandex.ru>
2023-01-20 18:07:03 +0200

    edit init.js

--- src/init.js
+++ src/init.js
@@ -1,6 +1,5 @@
 import i18next from 'i18next';
 import ru from './locales/ru.js';
-
 import app from './app.js';
 
 export default () => {
@@ -22,6 +21,14 @@ export default () => {
         feeds: [],
         posts: [],
       };
-      app(state, i18nextInstance);
+
+      const elements = {
+        form: document.querySelector('.rss-form'),
+        input: document.querySelector('#url-input'),
+        submit: document.querySelector('button[type="submit"]'),
+        feedback: document.querySelector('.feedback'),
+      };
+
+      app(state, elements, i18nextInstance);
     });
 };
